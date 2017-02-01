ca3ef34b625df980fe1ca4d87ee6568c27415d7f
David Cabeza <davidecl@hotmail.com>
2017-02-01 07:10:04 -0400

    Reparados algunos errores al compilar y cambios mayores.
    Este es el primer commit de todos los archivos.
    Los cambios que se describen aquí son de una versión del archivo que no está disponible porque el repositorio esta recién creado.
    
    pscheduler.c:
    
    - Ya se fusionó el código de lectura.c, se cambio en el PID int por long.
    - Se elimino todo el codigo repetido en las funciones InsertarProceso, ProximoProceso e Imprimir, para esto se crearon las funciones auxiliares InsertarProc, ProximoProc e Imprimir que son llamadas con cada cola.
    - El estilo de la documentación es parecido al de JAVADOC. Doxygen es una herramienta muy conocida para generar informes de codigo en C y reconoce este estilo.
    
    pscheduler.h:
    - Se agrego el atributo "nro" a la cola para identificarla y mostrar el numero de la cola en la función de Imprime.
    - Se hizo la definción del tipo Estado (typedef Estado) y se cambió el código que apuntaba a la vieja estructura "Estado"
    - En la estructura NODO se cambio el tipo de next por void* debido a que no reconoce NODO. Parece seguir sin funcionar
    - Se incluye la libreria <string.h> para utilizar la funcion strcpy
    
    Ambos archivos siguen teniendo errores de compilación.
    Comando para compilar enlazando pscheduler.h con pscheduler.c: gcc pscheduler.h pscheduler.c -o pscheduler
    
    tracker:
    - Archivo para escribir cambios que se han hecho, cosas por hacer y dudas por preguntar.
    - Formato sugerido para indicar cambios, sugerencias, errores, dudas, cosas por hacer:
    
    	@ nombre_funcion() <- Indicar en la funcion "nombre_funcion"
    	struct nombre_estructura <- Indicar la estructura "nombre_estructura"
    	100 codigo <- Indicar en la linea 100 en el codigo "codigo"
    
    < En construccion >
    
    datos:
    - Este es el ejemplo de formato del archivo de entrada del programa.

---
+++ pscheduler.c
@@ -0,0 +1,241 @@
+/*****************************************************************************
+* Universidad Simon Bolivar
+* Departamento de Computacion y Tecnologia de la Informacion
+* CI3825: Laboratorio de Sistemas Operativos 
+*
+* Tarea 1
+*
+* Objetivo: Adquisicion de destrezas de programacion en el lenguaje C
+*
+* Autores: 
+*   - David Cabeza 13-10191
+*   - Fabiola Martinez 13-10838
+* Equipo: 
+*
+* Prof. Angela Di Serio
+*
+* Febrero, 2017.
+*****************************************************************************/
+
+/*
+	Este es el programa principal.
+*/
+
+#include "pscheduler.h"
+
+int main(int argc, char const *argv[])
+{
+	char *filename;
+	strcpy(filename, argv[1]);
+
+	EstrucSched *estructura = Construye(filename);
+
+	Imprime(estructura);
+
+	return 0;
+}
+
+EstrucSched* Construye(char *filename){
+
+	EstrucSched *E = (EstrucSched *) malloc(sizeof(EstrucSched));
+	init_EstrucSched(E);
+
+	long PID;
+	char Estado;
+	short Prioridad;
+	float Time;
+	char Comando[100];
+
+	FILE *fp = fopen(filename, "r");
+
+	while(fscanf(fp, "%li %c %hi %f %s", &PID, &Estado, &Prioridad, &Time, Comando) != EOF){
+
+		printf("%d\t", PID);
+		printf("%c\t", Estado);
+		printf("%hi\t", Prioridad);
+		printf("%.2f\t", Time);		 // .2 cantidad de decimales
+		printf("%s\n", Comando);
+		
+		Proceso *p = (Proceso *) malloc(sizeof(Proceso));
+		init_proceso(p, PID, Time, Estado, Comando);
+		InsertarProceso(E, p, Prioridad);
+	}
+
+	return E;
+	
+}
+
+void InsertarProceso(EstrucSched* s, Proceso *p, short prioridad){
+	
+	switch(prioridad){
+		case '0':
+			insertarProc(s->q0, p);
+		case '1':
+			insertarProc(s->q1, p);
+		case '2':
+			insertarProc(s->q2, p);
+		case '3':
+			insertarProc(s->q3, p);
+		case '4':
+			insertarProc(s->q4, p);
+		case '5':
+			insertarProc(s->q5, p);
+	}
+}
+
+void insertarProc(COLA *q, Proceso *p){
+	
+	NODO *n = (NODO *) malloc(sizeof(NODO));
+	init_nodo(n, p);
+	
+	if(!q->primero){
+		q->primero = n;
+		q->ultimo = n;
+	}
+
+	else{
+		q->ultimo->next = (NODO *) n;
+		q->ultimo = n;
+	}
+
+	q->size++;
+}
+
+/* Devuelve el proximo proceso a planificar.
+   La rutina comienza buscando por la cola de mayor prioridad (q0) un proceso 
+   que este en estado Listo. Si es encontrado, se coloca al final de su cola y se
+   cambia su estado a En Ejecucion. Si ya existe un proceso en ejecucion, este 
+   es detenido para planificar el otro. Si no encuentra ningun proceso,
+   devuelve NULL.
+
+  Ejemplo de un proceso detenido para planificar otro:
+  	q0: E(0) L(1) L(2) L(3) **
+  	ProxProceso(s)
+  	q0: L(0) L(2) L(3) E(1)
+
+  ** Se supone que los procesos 1, 2 y 3 fueron insertados a q0 mediante la interfaz
+  de usuario. El proceso 0 teoricamente siempre fue el ultimo de la cola.
+*/
+Proceso *ProxProceso(EstrucSched *s){
+	
+	Proceso *p;
+
+	p = ProxProc(s, s->q0);
+
+	if(p) return p;
+
+	p = ProxProc(s, s->q1);
+
+	if(p) return p;
+
+	p = ProxProc(s, s->q2);
+
+	if(p) return p;
+
+	p = ProxProc(s, s->q3);
+
+	if(p) return p;
+
+	p = ProxProc(s, s->q4);
+
+	if(p) return p;
+
+	p = ProxProc(s, s->q5);
+
+	if(p) return p;
+
+	return NULL;
+
+}
+
+Proceso *ProxProc(EstrucSched *s, COLA *q){
+
+	NODO *n = q->primero;
+	Proceso *p = n->proceso;
+
+	// Teoricamente si los elementos de la cola son más de 1 y el primero 
+	// "no esta listo", el segundo lo esta.
+	// Ejemplo: E(0) L(1) L(2) -> L(0) L(2) E(1)
+	// Ejemplo: L(0) L(1) L(2) -> L(1) L(2) E(0)
+	// Ejemplo: L(0) -> E(0) 
+	
+	if(n != NULL && p->Estado == 'L'){
+		// CAMBIAR EL ESTADO CON LA FUNCION CambiarEstado?
+		proceso->Estado = 'E';
+		q->primero = n->next;
+		q->ultimo->next = n;
+		q->ultimo = n;
+		s->enEjecucion = n;
+		return p;
+	}
+
+	return NULL;
+}
+
+void Imprime(EstrucSched *s){
+	
+	(s->q0->size != 0) Imprmir(s->q0);
+
+	(s->q1->size != 0) Imprimir(s->q1);		
+
+	(s->q2->size != 0) Imprimir(s->q2);
+		
+	(s->q3->size != 0) Imprimir(s->q3);
+
+	(s->q4->size != 0) Imprimir(s->q4);
+
+	(s->q5->size != 0) Imprimir(s->q5);
+}
+
+void Imprimir(COLA *q){
+
+	NODO *first = q->primero;
+	printf("Cola %d", q->nro);
+	printf("PID\tEstado\tTiempo\tComando\n");
+	printf("%li\t%c\t%f\t%s\n", first->proceso.PID, first->proceso.Estado, first->proceso.Time, first->proceso.Comando);
+	
+	NODO *proximo = first->next;
+
+	while(proximo != NULL){
+		printf("%li\t%c\t%f\t%s\n", proximo->proceso.PID, proximo->proceso.Estado, proximo->proceso.Time, proximo->proceso.Comando);
+		proximo = proximo->next;
+	}
+}
+
+void init_proceso(Proceso *p, long procesoid, float time, char estado, char* comando){
+	p->PID = procesoid;
+	p->Time = time;
+	P->Estado = estado;
+	strcpy(p->Comando, comando);
+}
+
+void init_nodo(NODO *n, Proceso *p){
+	n->proceso = p;
+	n->next = NULL;
+}
+
+void init_cola(COLA *q, int numero){
+	q->primero = NULL;
+	q->ultimo = NULL;
+	q->size = 0;
+	q->nro = numero;
+}
+
+void init_EstrucSched(EstrucSched *E){
+	E->q0 = (COLA *) malloc(sizeof(COLA));
+	E->q1 = (COLA *) malloc(sizeof(COLA));
+	E->q2 = (COLA *) malloc(sizeof(COLA));
+	E->q3 = (COLA *) malloc(sizeof(COLA));
+	E->q4 = (COLA *) malloc(sizeof(COLA));
+	E->q5 = (COLA *) malloc(sizeof(COLA));
+
+	E->enEjecucion = NULL;
+
+	init_cola(E->q0, 0);
+	init_cola(E->q1, 1);
+	init_cola(E->q2, 2);
+	init_cola(E->q3, 3);
+	init_cola(E->q4, 4);
+	init_cola(E->q5, 5);
+
+}
---
+++ pscheduler.h
@@ -0,0 +1,90 @@
+/*****************************************************************************
+* Universidad Simon Bolivar
+* Departamento de Computacion y Tecnologia de la Informacion
+* CI3825: Laboratorio de Sistemas Operativos 
+*
+* Tarea 1
+*
+* Objetivo: Adquisicion de destrezas de programacion en el lenguaje C
+*
+* Autores: 
+*   - David Cabeza 13-10191
+*   - Fabiola Martinez 13-10838
+* Equipo: 
+*
+* Prof. Angela Di Serio
+*
+* Febrero, 2017.
+*****************************************************************************/
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/*
+
+Este archivo contiene la definción de las estructuras y los prototipos de las
+funciones.
+
+*/
+
+typedef char* Estado;
+
+typedef struct proceso
+{
+	long PID;
+	float Time;
+	char Comando[100];
+	Estado Estado;
+} Proceso;
+
+typedef struct nodo
+{
+	Proceso *proceso;
+	void* *next;
+} NODO;
+
+typedef struct cola
+{
+	NODO *primero, *ultimo;
+	int size;
+	int nro;
+} COLA;
+
+typedef struct EstrucSched
+{
+	COLA *q0, *q1, *q2, *q3, *q4, *q5;
+	NODO *enEjecucion;
+
+} EstrucSched;
+
+// Prototipos de funciones principales 
+
+void InsertarProceso(EstrucSched *s, Proceso *p, short prioridad);
+
+void ElimProceso(EstrucSched *s, long pid, short prio);
+
+void ElimProcesoE(EstrucSched *s);
+
+Proceso *ProxProceso(EstrucSched *s);
+
+void CambiarEstado(EstrucSched *s, Proceso* p, Estado newestado);
+
+EstrucSched *Construye(char *filename);
+
+void Imprime(EstrucSched *s);
+
+// Prototipos de funciones secundarias
+
+void insertarProc(COLA *q, Proceso *p);
+
+Proceso *ProxProc(EstrucSched *s, COLA *q);
+
+void Imprimir(COLA *q);
+
+void init_proceso(Proceso *p, long procesoid, float time, char estado, char* comando);
+
+void init_nodo(NODO *n, Proceso *p);
+
+void init_cola(COLA *q);
+
+void init_EstrucSched(EstrucSched *E);
